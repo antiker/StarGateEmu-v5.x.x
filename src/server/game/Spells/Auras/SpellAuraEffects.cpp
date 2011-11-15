1ceee585e2bfff2849c9c5c599cb7f1a557ee06b
antiker <krallekalle@web.de>
2011-11-15 03:34:52 +0100

    Core/Spells Fixed #2

--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -6085,6 +6085,18 @@ void AuraEffect::HandleAuraDummy(AuraApplication const *aurApp, uint8 mode, bool
                     if (caster)
                         caster->CastSpell(caster, 13138, true, NULL, this);
                     break;
+    				// Guardian of Ancient Kings - Retribution
+                case 86698:
+                {
+                    caster->CastSpell(caster,86701,true);
+                    break;
+                }
+                    // Guardian of Ancient Kings - Holy
+                case 86669:
+                {
+                    caster->CastSpell(caster,86674,true);
+                    break;
+                }
                 case 34026:   // kill command
                 {
                     Unit *pet = target->GetGuardianPet();
@@ -6377,6 +6389,23 @@ void AuraEffect::HandleAuraDummy(AuraApplication const *aurApp, uint8 mode, bool
                     if (GetId() == 61777)
                         target->CastSpell(target, GetAmount(), true);
                     break;
+					case SPELLFAMILY_PALADIN:
+                {
+                    switch (GetId())
+                    {
+                        // Guardian of Ancient Kings - Retribution
+                        case 86698:
+                        {
+                            if (aurApp->GetBase()->GetOwner()->ToUnit()->HasAura(86700))
+                            {
+                                caster->CastSpell((Unit*)NULL,86704,true);
+                                caster->RemoveAura(86701);
+                                caster->RemoveAura(86700);
+                            }
+                            break;
+                        }
+                    }
+                }
             }
         }
     }
